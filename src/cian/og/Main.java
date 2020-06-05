500833bd8fbf83c32609d27c5753f5c7ba5bc5c6
Cian O'Gralaigh <ihatecian@gmail.com>
2020-06-05 17:34:08 +0100

    Add field and constructor

--- src/cian/og/Main.java
+++ src/cian/og/Main.java
@@ -3,17 +3,32 @@ package cian.og;
 public class Main {
 
     public static void main(String[] args) {
-	Car car = new Car();
+	Car car = new Car("BMW");
+	Car myOtherCar = new Car("Volkswagen");
 	car.accelerate();
+	myOtherCar.brake();
     }
 }
 
 class Car {
-    public void accelerate(){
-        System.out.println("You are going faster.");
+
+    private int speed = 0;
+    private String name;
+
+    public Car(String carName) {
+        name = carName;
     }
 
-    public void brake(){
-        System.out.println("You are going slower.");
+    public void accelerate(){
+        speed++;
+        System.out.printf("%s is going %d km%n",name, speed);
+    }
+
+    public void brake() {
+        if (speed == 1) {
+            speed--;
+        }
+
+        System.out.printf("%s is going %d km%n", name, speed);
     }
 }
